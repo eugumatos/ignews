b8a4ff77c58497611a1895f21a072fdc5db73016
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-04-12 20:42:29 -0300

    Integration with stripe, faunadb and creation of authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton () {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Gustavo Matos
+      {session.user.name}
       <FiX color="#73780" className={styles.closeIcon} />
     </button>
   ) : (
     <button 
-    type="button"
-    className={styles.signInButton}
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn()}
     >
       <FaGithub color="#eba417" />
       Sign In with Github
