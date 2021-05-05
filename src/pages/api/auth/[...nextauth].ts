61db1e5158329dda566703bf4e811e8d231a4998
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-05-04 21:32:36 -0300

    Finished app

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,12 @@ import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/faunadb';
 
+declare module "next-auth" {
+  interface Session {
+    activeSubscription: object | null;
+  }
+}
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -14,6 +20,44 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+
+     
+    },
     async signIn(user, account, profile) {
       const { email } = user;
       
